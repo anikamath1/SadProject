9fd9eed2a0af10521c810fc60922b83416e1c12a
Anirudh Kamath <Anirudh Kamath>
2018-09-11 00:09:16 +0530

    CHECK THIS OUT

---
+++ AndroidStudioProjects/restaurant-master/app/src/main/java/com/example/android/restuarantfinder/menuMaoStarter.java
@@ -0,0 +1,30 @@
+package com.example.android.restuarantfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.List;
+
+public class menuMaoStarter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_menu_mao_starter );
+
+
+        ListView listViewStarter = (ListView) findViewById(R.id.listViewStarter);
+        dataBaseAccess databaseAccess = dataBaseAccess.getInstance(getApplicationContext());
+        databaseAccess.open();
+
+        Intent intent = getIntent();
+        String buttonText = intent.getStringExtra("itemType");
+        List<String> itemName = databaseAccess.getStartersMao(buttonText);
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemName);
+        listViewStarter.setAdapter(adapter);
+    }
+}
